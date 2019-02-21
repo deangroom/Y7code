bd2866f264dd2cafbef8d900861a2353c4c2f2a5
deangroom <deangroom@gmail.com>
2019-02-22 08:24:00 +1100

    Add files via upload

---
+++ _03_Int.ino
@@ -0,0 +1,11 @@
+int led = 12;             // tell Arduino that pin 12 is going to be called “led” for the following sketch
+void setup() {
+pinMode(led, OUTPUT);     // initialize digital pin 12 as an output.
+}
+void loop() {             // the loop function runs over and over again forever
+digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
+delay(1000);              // wait for a second
+digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
+delay(1000);              // wait for a second
+}
+
---
+++ _04_fade.ino
@@ -0,0 +1,17 @@
+int led = 11;                             // the pin the LED is attached to (must be a PWM output indicated on board by the ~ symbol). 
+int brightness = 0;                       // how bright the LED is 
+int fadeAmount = 5;                       // how many points to fade the LED by
+                                          // the setup routine runs once when you press reset:
+void setup() {
+pinMode(led, OUTPUT);                     // declare pin led to be an output:
+}
+
+void loop() {                                   // the loop routine runs over and over again forever:
+analogWrite(led, brightness);                   // set the brightness of pin 9: 
+brightness = brightness + fadeAmount;           // change the brightness for next time through the loop:
+if (brightness <= 0 || brightness >= 255) {     // reverse the direction of the fading at the ends of the fade:
+fadeAmount = -fadeAmount; }
+delay(30);                                      // wait for 30 milliseconds to see the dimming effect
+}
+
+
